6ef0fed43c158de5953ad93892f6eccf53c1660e
CHOAIB EL KAOURI <65832846+Choaibelkaouri@users.noreply.github.com>
2025-11-17 21:08:36 +0100

    Update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -5,6 +5,23 @@ const Home = () => {
    </div>
   )
 }
+import type { ReactNode } from "react";
+
+export interface PageRouteProps {
+  pageRoute: string;
+}
+
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
+  action?: () => void;
+}
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
 
 export default Home;
 
